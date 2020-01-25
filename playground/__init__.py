c7634788f38896ec58ef660640da7dddb7a3ae23
Söhnke Thorben Braun <apple1992A>
2020-01-25 14:12:06 +0100

    Use Seaborn for viz

--- playground/__init__.py
+++ playground/__init__.py
@@ -1,39 +1,45 @@
 from env.RoRoDeck import RoRoDeck
 from agent.TDQLearning import TDQLearning
 import pandas as pd
-import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
-import tqdm
-import time
+
 
 env = RoRoDeck()
-agent = TDQLearning(100000)
+agent = TDQLearning(1000)
 q_table, totalRewards, stateExpantion, stepsToExit = agent.train(env)
 
 print("Rewards Max:")
 print(max(totalRewards))
 
-
-smoothing_window = 40
+sns.set(style="darkgrid")
+smoothing_window = 80
 fig2 = plt.figure(figsize=(10, 5))
 rewards_smoothed = pd.Series(totalRewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+ax = sns.lineplot(data=rewards_smoothed, linewidth=2.5, dashes=False,color="blue")
 plt.plot(rewards_smoothed)
 plt.xlabel("Episode")
 plt.ylabel("Episode Reward (Smoothed)")
 plt.title("Episode Reward over Time (Smoothed over window size {})".format(smoothing_window))
-plt.show()
+#plt.show()
 
 
-print("State Expansion MAx:")
-print(max(stateExpantion))
+
+
 #plt.title("State Expansion")
-fi3 = plt.figure(figsize=(50, 30))
-plt.plot(stateExpantion)
-plt.show()
+fi3 = plt.figure(figsize=(10, 5))
+ax = sns.lineplot(data=pd.Series(stateExpantion), linewidth=2.5, dashes=False,color="black")
+plt.xlabel("Episode")
+plt.ylabel("States Explored")
+plt.title("State Expansion over time")
+#plt.show()
 
-print("Steps to Exit:")
-print(min(stepsToExit))
-#plt.title("Steps to Exit")
-plt.figure(figsize=(50, 30))
-plt.plot(stepsToExit)
+
+#Plot smoothed Steps to Exit
+steps_smoothed = pd.Series(stepsToExit).rolling(int(smoothing_window/2), min_periods=int(smoothing_window/2)).mean()
+fi4 = plt.figure(figsize=(10, 5))
+ax = sns.lineplot(data=steps_smoothed, linewidth=2.5, dashes=False, color="green")
+plt.xlabel("Episode")
+plt.ylabel("Steps to Finish (Smoothed)")
+plt.title("Steps to Finish over Time (Smoothed over window size {})".format(int(smoothing_window/2)))
 plt.show()

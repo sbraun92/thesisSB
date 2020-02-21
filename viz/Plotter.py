69e2ec349f97117959dec45ee2047b91fa93058f
Söhnke Thorben Braun <apple1992A>
2020-02-21 15:47:43 +0100

    minor

--- viz/Plotter.py
+++ viz/Plotter.py
@@ -12,7 +12,9 @@ class Plotter(object):
         self.stateExpPlot = stateExpPlot
         self.stepPlot = stepPlot
         self.it = it
-        self.smoothingWindow = int(it/50)
+        self.smoothingWindow = int(it/100)
+        if self.smoothingWindow == 0:
+            self.smoothingWindow = 1
         logging.getLogger('log1').info("Setting path for plots to:"+ path)
         self.path = path
 

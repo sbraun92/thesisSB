cda07fdb784895e61a9bacf142d1e11270b3cb36
Söhnke Thorben Braun <apple1992A>
2020-03-06 19:19:08 +0100

    minor in Test

--- tests/env_test.py
+++ tests/env_test.py
@@ -5,13 +5,10 @@ import pytest
 def test_RORODeck():
     env = RoRoDeck(False)
     env.reset()
-    env.step(1)
-
     done = False
     i = 0
     while(not done):
-        env.render()
-        observation_, reward, done, info = env.step(0)
+        observation_, reward, done, info = env.step(env.actionSpaceSample())
         i+=1
         assert i <= 100
 

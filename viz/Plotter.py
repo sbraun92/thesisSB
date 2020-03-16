587be661721c4c730444f09647646085086c7617
Söhnke Thorben Braun <apple1992A>
2020-03-16 14:45:16 +0100

    added plotter method for Epsilon development

--- viz/Plotter.py
+++ viz/Plotter.py
@@ -6,11 +6,12 @@ import logging
 sns.set(style="darkgrid")
 
 class Plotter(object):
-    def __init__(self, path, it, rewardPlot = True, stateExpPlot = True, stepPlot=True):
+    def __init__(self, path, it, rewardPlot = True, stateExpPlot = True, stepPlot=True, epsHistory=True):
         logging.getLogger('log1').info("Initialise Plotting Unit")
         self.rewardPlot = rewardPlot
         self.stateExpPlot = stateExpPlot
         self.stepPlot = stepPlot
+        self.epsHistory = epsHistory
         self.it = it
         self.smoothingWindow = int(it/200)
         #self.smoothingWindow = 200
@@ -19,13 +20,15 @@ class Plotter(object):
         logging.getLogger('log1').info("Setting path for plots to:"+ path)
         self.path = path
 
-    def plot(self, rewards, states, steps):
+    def plot(self, rewards, states, steps, eps_history):
         if self.rewardPlot == True:
             self.plotRewardPlot(rewards)
         if self.stateExpPlot is True:
             self.plotStateExp(states)
         if self.stepPlot is True:
             self.plotStepPlot(steps)
+        if self.epsHistory is True:
+            self.plotEPSHistory(eps_history)
 
     def plotRewardPlot(self,totalRewards):
         logging.getLogger('log1').info("prepare reward plot...")
@@ -34,7 +37,7 @@ class Plotter(object):
         rewards_q75 = pd.Series(totalRewards).rolling(self.smoothingWindow, min_periods=self.smoothingWindow).quantile(0.75)
         rewards_q25 = pd.Series(totalRewards).rolling(self.smoothingWindow, min_periods=self.smoothingWindow).quantile(0.25)
         rewardsDf = pd.DataFrame(totalRewards)
-        ax = sns.lineplot(data=rewards_smoothed, linewidth=2.5, dashes=False, )
+        ax = sns.lineplot(data=rewards_smoothed, linewidth=2.5, dashes=False)
 
         #ax = sns.lineplot(data=rewards_smoothed+rewards_std, linewidth=2.5, dashes=False,ax=ax)
 
@@ -75,4 +78,13 @@ class Plotter(object):
         fi4.savefig(self.path + '_StepsToFinish.png')
         logging.getLogger('log1').info("finished plot")
 
-
+    def plotEPSHistory(self,eps_history):
+        logging.getLogger('log1').info("prepare state Epsilon plot...")
+        fi3 = plt.figure(figsize=(10, 5))
+        ax = sns.lineplot(data=pd.Series(eps_history), linewidth=2.5, dashes=False, color="blue")
+        plt.xlabel("Episode")
+        plt.ylabel("Epsilon development")
+        plt.title("Epsilon Development per episode")
+        # plt.show()
+        fi3.savefig(self.path + '_EPSDevelopment.png')
+        logging.getLogger('log1').info("finished plot")

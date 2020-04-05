68d59e8f182b03ac62028bd2941787e2142b89d9
Söhnke Thorben Braun <apple1992A>
2020-04-05 17:38:24 +0200

    modified test

--- tests/tdq_agent_test.py
+++ tests/tdq_agent_test.py
@@ -1,27 +1,26 @@
 from env.roroDeck import RoRoDeck
 from agent.TDQLearning import TDQLearning
+from agent.SARSA import SARSA
 import pytest
 import numpy as np
 
 np.random.seed(0)
 
-def test_agent():
+def test_TDQagent():
     env = RoRoDeck(False)
     env.rows = 12
     env.lanes = 12
     env.reset()
-
     agent = TDQLearning(env,None)
 
     assert len(agent.q_table.keys()) == 0
-
     agent.numGames=1
-
     agent.train()
 
-    #env.numGames = 1
-
     assert len(agent.q_table.keys()) == 48
+
+
+
 
 def test_max_action_method():
     env = RoRoDeck(False)
@@ -30,9 +29,6 @@ def test_max_action_method():
     state = env.reset()
 
     agent = TDQLearning(env, None)
-
-    #agent.numGames = 1
-    #agent.train()
 
     agent.q_table[state.tobytes()] = np.zeros(4)
 
@@ -46,3 +42,37 @@ def test_max_action_method():
 
     assert agent.maxAction(agent.q_table, state, None) == 2
 
+
+def test_SARSAagent():
+    env = RoRoDeck(False)
+    env.rows = 12
+    env.lanes = 12
+    env.reset()
+    agent = SARSA(env, None)
+
+    assert len(agent.q_table.keys()) == 0
+    agent.numGames = 1
+    agent.train()
+
+    assert len(agent.q_table.keys()) == 48
+
+
+def test_max_action_method():
+    env = RoRoDeck(False)
+    env.rows = 12
+    env.lanes = 12
+    state = env.reset()
+
+    agent = SARSA(env, None)
+
+    agent.q_table[state.tobytes()] = np.zeros(4)
+
+    assert np.count_nonzero(agent.q_table[state.tobytes()]) == 0
+    agent.q_table[state.tobytes()][2] = 1
+    agent.q_table[state.tobytes()][3] = 2
+
+    assert agent.maxAction(agent.q_table,state,None) == 3
+
+    env.possibleActions = np.array([0,1,2])
+
+    assert agent.maxAction(agent.q_table, state, None) == 2

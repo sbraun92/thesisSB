8d327a46cbc8b65fca3088a8f19372add723bb2f
Söhnke Thorben Braun <apple1992A>
2020-10-03 01:36:21 +0200

    worked on loggingUnit.py

--- analysis/loggingUnit.py
+++ analysis/loggingUnit.py
@@ -8,21 +8,19 @@ class LoggingBase(object):
         date = str(datetime.utcnow().date().strftime("%Y%m%d"))
         time = str(datetime.now().strftime("%H%M"))
 
-        self.module_path = str(os.path.dirname(os.path.realpath(__file__))) + '\\out\\' + date + '\\' +time +'\\'
+        self.module_path = str(os.path.dirname(os.path.realpath(__file__))) + '\\out\\' + date + '\\' + time + '\\'
         os.makedirs(self.module_path, exist_ok=True)
 
         self.module_path += time
 
+        logging.basicConfig(filename=self.module_path + '_log.log', level=logging.INFO,
+                            format="%(asctime)s - %(levelname)s - %(message)s")
+        #handler = logging.StreamHandler()
+        #handler.setLevel(logging.INFO)
+        # formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-        logging.basicConfig(filename=self.module_path + '_log.log', level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")
-        handler = logging.StreamHandler()
-        handler.setLevel(logging.INFO)
-        #formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        # self.logger1 = logging.getLogger('log1')
 
+        # self.logger1.addHandler(logging.FileHandler(self.module_path + '_debugger.log'))
 
-        #self.logger1 = logging.getLogger('log1')
-
-
-        #self.logger1.addHandler(logging.FileHandler(self.module_path + '_debugger.log'))
-
-        logging.getLogger('log1').info("Initialise Logger to %s", self.module_path)
+        logging.getLogger(__name__).info("Initialised Logger to {}".format(self.module_path))
